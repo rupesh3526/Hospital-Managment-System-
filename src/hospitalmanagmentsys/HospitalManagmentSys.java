dc1fb035fb3c17f7a2149d19a98002ef0e32f821
rupesh3526 <rupeshsaini3526@gmail.com>
2025-09-20 11:33:15 +0530

    Menu created

--- src/hospitalmanagmentsys/HospitalManagmentSys.java
+++ src/hospitalmanagmentsys/HospitalManagmentSys.java
@@ -8,18 +8,58 @@ public class HospitalManagmentSys {
         Scanner scanner = new Scanner(System.in);
         Dao dao = new DaoImplementaion();
 
-        Patient p1 = new Patient();
-        p1.setPatientAge(10);
-        p1.setPatientGender("male");
-        p1.setPatientName("Rahul");
-       /* dao.addPatient(p1);
-        dao.viewPatients();*/
-       // dao.getPatientById(2);
-       // dao.removePatient(3);
-        //dao.viewDoctors();
-        dao.bookAppoinment(dao,scanner);
+        while (true) {
+            System.out.println("\n=== Hospital Management System ===");
+            System.out.println("1. Add Patient");
+            System.out.println("2. View Patients");
+            System.out.println("3. Remove Patient");
+            System.out.println("4. View Doctors");
+            System.out.println("5. Book Appointment");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+
+            int num = scanner.nextInt();
+            scanner.nextLine(); // consume leftover newline after nextInt()
+
+            switch (num) {
+                case 1:
+                    Patient p1 = new Patient();
+                    System.out.print("Enter Name of Patient: ");
+                    p1.setPatientName(scanner.nextLine());
+
+                    System.out.print("Enter Gender: ");
+                    p1.setPatientGender(scanner.nextLine());
+
+                    System.out.print("Enter Age: ");
+                    p1.setPatientAge(scanner.nextInt());
+
+                    dao.addPatient(p1);
+                    break;
+
+                case 2:
+                    dao.viewPatients();
+                    break;
+
+                case 3:
+                    System.out.print("Enter Patient Id to Remove: ");
+                    dao.removePatient(scanner.nextInt());
+                    break;
+
+                case 4:
+                    dao.viewDoctors();
+                    break;
+
+                case 5:
+                    dao.bookAppoinment(dao, scanner);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting... Goodbye!");
+                    return; // break the while loop & exit program
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
-
-
 }
-
